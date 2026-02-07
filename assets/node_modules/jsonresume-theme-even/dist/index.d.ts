1c38820b3d11996a5526dc5028a8cca3ff42464b
yohei-ohata <yohei.ohata.ee@gmail.com>
2026-02-07 23:16:56 +0900

    chore: remove node_modules from tracking

--- assets/node_modules/jsonresume-theme-even/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export namespace pdfRenderOptions {
-    let mediaType: string;
-    let printBackground: boolean;
-}
-export function render(resume: import('./schema.d.ts').ResumeSchema): string;
-//# sourceMappingURL=index.d.ts.map
